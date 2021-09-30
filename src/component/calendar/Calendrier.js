8e9d8f26e4b1d5dcc818d409eb2bf798ef30ce1c
Votre Nom <Vous@exemple.com>
2021-10-01 00:25:04 +0200

    add buttons change color

--- src/component/calendar/Calendrier.js
+++ src/component/calendar/Calendrier.js
@@ -222,23 +222,6 @@ const customStyles = {
         })
     }}
 
-    const handleChangeColor = (id, color) => {
-      axios({
-        method: 'put',
-        url:`${process.env.REACT_APP_API_URL}api/color/${id}`,
-        'Access-Control-Allow-Credentials': true,
-        data: {color}
-      })
-      .then(res => {
-        if(res.data) {
-          console.log(res.data);
-        };
-      })
-      .catch(err => {
-        console.log(err);
-      })
-    }
-
     const handleCreateUser = e => {
       e.preventDefault()
       if(pseudo.length >= 3 ){
@@ -377,7 +360,7 @@ const customStyles = {
                     <div>
                           <h2>{detailView.name}</h2>
                           <div >
-                            <button style={{ backgroundColor : '#7b7a7a'}}  onClick={() => handleChangeColor( detailView._id, "#7b7a7a") }>Annuler</button>
+                            <button style={{ backgroundColor : '#7b7a7a'}}  onClick={() => dispatch(editColor(detailView._id, "#7b7a7a" )) }>Annuler</button>
                             <button>Attente</button>
                             <button style={{ backgroundColor : 'yellow' }} >Arriver</button>
                           </div>
